f1d460e69889dd3a13d871e351e56fa446d81b6e
Naing Lin Aung <nainglinaung@Naings-Mac-mini.local>
2022-06-29 19:32:33 +0630

    initial commit

---
+++ WeatherFetch/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  WeatherFetch
+//
+//  Created by Naing Lin Aung on 6/29/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var countryNameLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var currentWeatherTypeImage: UIImageView!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentWeatherConditionLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windSpeedPerHrLabel: UILabel!
+    @IBOutlet weak var realFeelLabel: UILabel!
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
